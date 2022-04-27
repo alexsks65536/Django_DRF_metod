30bf1d75c339882767ac0d0d6508fae58124692d
alexsks65536 <megbyte@mail.ru>
2022-04-27 21:02:27 +0300

    lesson_06 method office

---
+++ library/authors/tests.py
@@ -0,0 +1,47 @@
+import json
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+from .views import AuthorModelViewSet
+from .models import Author, Book
+
+
+class TestAuthorViewSet(TestCase):
+
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/authors/')
+        view = AuthorModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_guest(self):
+        factory = APIRequestFactory()
+        request = factory.post('/api/authors/', {'name': 'Пушкин', 'birthday_year': 1799}, format='json')
+        view = AuthorModelViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_admin(self):
+        factory = APIRequestFactory()
+        request = factory.post('/api/authors/', {'name': 'Пушкин', 'birthday_year': 1799}, format='json')
+        admin = User.objects.create_superuser('admin1', 'admin@admin.com', 'admin')
+        force_authenticate(request, admin)
+        view = AuthorModelViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_detail(self):
+        author = Author.objects.create(name='Пушкин', birthday_year=1799)
+        client = APIClient()
+        response = client.get(f'/api/authors/{author.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_edit_guest(self):
+        author = Author.objects.create(name='Пушкин', birthday_year=1799)
+        client = APIClient()
+        response = client.put(f'/api/authors/{author.id}/', {'name':'Грин',
+        'birthday_year': 1880})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
